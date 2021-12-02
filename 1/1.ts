38fba92e88c2c73df09abf7afe84202c34c2300b
Kyle Warren <qarren@gmail.com>
2021-12-02 08:42:26 -0800

    reorganization

--- 1/1.ts
+++ 1/1.ts
@@ -2,18 +2,15 @@ import {
   decodeText,
   chunkLines,
   map,
-  rollingWindow,
-} from './iter.ts';
+} from '../iter.ts';
 
-const file = await Deno.open('./input');
+const file = await Deno.open(new URL('input', import.meta.url));
 
 let iter = Deno.iter(file);
 
 let textIter = decodeText(iter);
 textIter = chunkLines(textIter);
 let numbers = map(textIter, parseInt);
-let windows = rollingWindow(numbers, 3);
-numbers = map(windows, sum);
 
 let count = 0;
 let prev = (await numbers.next()).value;
@@ -27,11 +24,3 @@ for await (const current of numbers) {
 console.log('count', count);
 
 file.close();
-
-function sum(array: Array<number>): number {
-  let value = 0;
-  for (const number of array) {
-    value += number;
-  }
-  return value;
-}
--- 1/2.ts
+++ 1/2.ts
@@ -2,15 +2,18 @@ import {
   decodeText,
   chunkLines,
   map,
-} from './iter.ts';
+  rollingWindow,
+} from '../iter.ts';
 
-const file = await Deno.open('./input');
+const file = await Deno.open(new URL('input', import.meta.url));
 
 let iter = Deno.iter(file);
 
 let textIter = decodeText(iter);
 textIter = chunkLines(textIter);
 let numbers = map(textIter, parseInt);
+let windows = rollingWindow(numbers, 3);
+numbers = map(windows, sum);
 
 let count = 0;
 let prev = (await numbers.next()).value;
@@ -24,3 +27,11 @@ for await (const current of numbers) {
 console.log('count', count);
 
 file.close();
+
+function sum(array: Array<number>): number {
+  let value = 0;
+  for (const number of array) {
+    value += number;
+  }
+  return value;
+}
