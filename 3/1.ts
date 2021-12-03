21b19f7f205d20cdf8c516faecbc841bfd2f0c6c
Kyle Warren <qarren@gmail.com>
2021-12-03 10:50:09 -0800

    did day 3

---
+++ 3/1.ts
@@ -0,0 +1,43 @@
+import { chunkLines, decodeText, map } from "../iter.ts";
+
+const file = await Deno.open(new URL("input", import.meta.url));
+
+let iter = Deno.iter(file);
+
+let textIter = decodeText(iter);
+textIter = chunkLines(textIter);
+
+const counts = fill([], 0, 12); 
+for await (const data of textIter) {
+  for (let i = 0; i < 12; i++) {
+    if (data[i] === '0') {
+      counts[i] -= 1;
+    } else {
+      counts[i] += 1;
+    }
+  }
+}
+
+let gamma = calcWeirdValue(
+  counts.map(value => value > 0 ? '1' : '0'),
+);
+
+let epsilon = calcWeirdValue(
+  counts.map(value => value > 0 ? '0' : '1'),
+);
+
+console.log('counts', counts);
+console.log('gamma', counts.map(value => value > 0 ? '1' : '0').join(''), gamma),
+console.log('epsilon', counts.map(value => value < 0 ? '1' : '0').join(''), epsilon),
+console.log('final', gamma * epsilon);
+
+function calcWeirdValue(bits: string[]): number {
+  return parseInt(bits.join(''), 2);
+}
+
+function fill<T>(array: Array<T>, value: T, size: number): Array<T> {
+  for (let i = 0; i < size; i++) {
+    array[i] = value;
+  }
+  return array;
+}
---
+++ 3/2.ts
@@ -0,0 +1,65 @@
+import {chunkLines, decodeText, map} from '../iter.ts';
+
+const file = await Deno.open(new URL('input', import.meta.url));
+
+let iter = Deno.iter(file);
+
+let textIter = decodeText(iter);
+textIter = chunkLines(textIter);
+
+const numbers = [];
+for await (const data of textIter) {
+  numbers.push(data);
+}
+
+const length = 12;
+
+const oxygenList = filterNumbers(numbers, (count) => (count >= 0 ? '1' : '0'));
+const co2List = filterNumbers(
+  numbers,
+  (count) => (count < 0 ? '1' : '0')
+);
+
+console.log('oxygen', oxygenList);
+console.log('co2', co2List);
+console.log('final', parseInt(oxygenList[0], 2) * parseInt(co2List[0], 2));
+
+function filterNumbers(
+  numbers: string[],
+  getDesiredBit: (count: number) => string,
+): string[] {
+  let list = numbers;
+  for (let i = 0; i < length; i++) {
+    let newList = [];
+    const desiredBit = getDesiredBit(getCount(list, i));
+    newList = list.filter((data) => data[i] === desiredBit);
+    if (newList.length === 0) {
+      break;
+    }
+    list = newList;
+  }
+  return list;
+}
+
+function getCount(numbers: string[], index: number): number {
+  let count = 0;
+  for (const data of numbers) {
+    if (data[index] === '0') {
+      count -= 1;
+    } else {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+function calcWeirdValue(bits: string[]): number {
+  return Number('0b' + bits.join(''));
+}
+
+function fill<T>(array: Array<T>, value: T, size: number): Array<T> {
+  for (let i = 0; i < size; i++) {
+    array[i] = value;
+  }
+  return array;
+}
