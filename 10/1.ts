5e38cd379dec3b28fb69e5168ed9c54b086c1ba7
Kyle Warren <qarren@gmail.com>
2021-12-10 09:56:24 -0800

    part 2 day 10

--- 10/1.ts
+++ 10/1.ts
@@ -3,38 +3,55 @@ import {decodeText, chunkLines} from '../iter.ts';
 const stdin = chunkLines(decodeText(Deno.iter(Deno.stdin)));
 
 const openChars = ['(', '[', '{', '<'];
-
 const closeChars = [')', ']', '}', '>'];
 
-const scores = new Map([
+const errorScores = new Map([
   [')', 3],
   [']', 57],
   ['}', 1197],
   ['>', 25137],
 ]);
 
+const completionScores = new Map(
+  openChars.map((char, index) => [char, index + 1]),
+);
+
 const openSet = new Set(openChars);
-const closeSet = new Set(closeChars);
 
-const closeToOpen = closeChars.reduce((map, char, index) => {
-  map.set(char, openChars[index]);
-  return map;
-}, new Map());
+const closeToOpen = new Map(
+  closeChars.map((char, index) => [char, openChars[index]]),
+);
 
-let score = 0;
+let errorScore = 0;
+const incompleteScores = [];
 for await (const line of stdin) {
   const scope = [];
 
-  for (const char of line) {
-    if (openSet.has(char)) {
-      scope.push(char);
-    } else if (scope.at(-1) === closeToOpen.get(char)) {
-      scope.pop();
-    } else {
-      score += scores.get(char) ?? 0;
-      break;
+  try {
+    for (const char of line) {
+      if (openSet.has(char)) {
+        scope.push(char);
+      } else if (scope.at(-1) === closeToOpen.get(char)) {
+        scope.pop();
+      } else {
+        errorScore += errorScores.get(char) ?? 0;
+        throw new Error('oops');
+      }
     }
+
+    let score = 0;
+    for (const char of scope.slice().reverse()) {
+      score = 5 * score + (completionScores.get(char) ?? 0);
+    }
+    incompleteScores.push(score);
+  } catch (_error) {
+    // pass
   }
 }
 
-console.log('score', score);
+incompleteScores.sort((a, b) => b - a);
+
+const finalScore = incompleteScores[Math.floor(incompleteScores.length / 2)];
+
+console.log('errorScore', errorScore);
+console.log('completionScore', finalScore);
