c40e565e99453145f5faf9ec0e45eb75254b76aa
Kyle Warren <qarren@gmail.com>
2021-12-16 11:12:26 -0800

    day 16 part 2

--- 16/code.ts
+++ 16/code.ts
@@ -13,7 +13,7 @@ const string = await reduce(stdin, (result, chunk) => result + chunk, '');
 // const string = 'EE00D40C823060';
 
 // example
-//const string = 'A0016C880162017C3686B18A3D4780';
+//const string = '9C0141080250320F1802104A08';
 
 const binary = Array.from(string.trim())
   .map((char) => leftPad(parseInt(char, 16).toString(2), 4))
@@ -22,14 +22,15 @@ const binary = Array.from(string.trim())
 console.log('string', string);
 console.log('binary', binary);
 
-const result = parsePacket(binary);
-console.log('result', result);
+const packet = parsePacket(binary);
+console.log('packet', packet);
 
-console.log('version count', countVersions(result));
+console.log('version count', countVersions(packet));
+
+console.log('result', resolvePacket(packet));
 
 type BasicPacket = {
   version: number;
-  type: number;
   size: number;
 };
 
@@ -39,6 +40,7 @@ interface LiteralPacket extends BasicPacket {
 }
 
 interface OperatorPacket extends BasicPacket {
+  type: 0 | 1 | 2 | 3 | 5 | 6 | 7;
   subPackets: Packet[];
 }
 
@@ -89,6 +91,7 @@ function parsePacket(data: string): Packet {
 
   return {
     version,
+    // @ts-ignore fix this
     type,
     subPackets,
     size: index,
@@ -128,4 +131,31 @@ function countVersions(packet: Packet): number {
     (acc, packet) => countVersions(packet) + acc,
     sum,
   );
+}
+
+function resolvePacket(packet: Packet): number {
+  if (packet.type === 4) {
+    return packet.value;
+  }
+
+  const subValues = packet.subPackets.map(resolvePacket);
+
+  switch (packet.type) {
+    case 0:
+      return subValues.reduce((sum, value) => sum + value, 0);
+    case 1:
+      return subValues.reduce((sum, value) => sum * value, 1);
+    case 2:
+      return Math.min(...subValues);
+    case 3:
+      return Math.max(...subValues);
+    case 5:
+      return subValues[0] > subValues[1] ? 1 : 0;
+    case 6:
+      return subValues[0] < subValues[1] ? 1 : 0;
+    case 7:
+      return subValues[0] === subValues[1] ? 1 : 0;
+    default:
+      throw new Error('Invalid packet type');
+  }
 }
