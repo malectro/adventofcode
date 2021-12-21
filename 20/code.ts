e43d9dab4444e0c3c35d3705d05a96fc11109a9e
Kyle Warren <qarren@gmail.com>
2021-12-20 16:00:17 -0800

    cleanup

--- 20/code.ts
+++ 20/code.ts
@@ -1,10 +1,6 @@
 import {
   decodeText,
   chunkLines,
-  reChunk,
-  take,
-  map,
-  reduce,
   range,
   toArray,
 } from '../iter.ts';
@@ -14,13 +10,13 @@ const lines = await toArray(chunkLines(decodeText(Deno.iter(Deno.stdin))));
 const decoder = lines.shift() ?? '';
 lines.shift();
 
-console.log('decoder', decoder);
+//console.log('decoder', decoder);
 
 let image = lines.map((line) =>
   Array.from(line).map((char) => (char === '#' ? 1 : 0)),
 );
 
-console.log('image', '\n', serializeImage(image), '\n');
+//console.log('image', '\n', serializeImage(image), '\n');
 
 let zeroSpace = 0;
 for (const _ of range(0, 50)) {
@@ -37,18 +33,11 @@ for (const _ of range(0, 50)) {
           key = (key << 1) + (image[i + k - 1]?.[j + l - 1] ?? zeroSpace);
         }
       }
-      /*
-      if (i === 0) {
-        //console.log('key', key, decoder[key]);
-      }
-      */
       row[j] = decoder[key] === '#' ? 1 : 0;
     }
   }
 
-  //console.log('row 1\n', newImage[0].join(''), '\n');
-  //console.log('done');
-  console.log('zoom', '\n', serializeImage(newImage), '\n');
+  //console.log('zoom', '\n', serializeImage(newImage), '\n');
 
   image = newImage;
   zeroSpace = (zeroSpace + 1) % 2;
