94613c930394ccd8a2b45cd68b50e62a60984e3e
Kyle Warren <qarren@gmail.com>
2021-12-27 09:13:49 -0800

    refactored utils function

--- 2015/src/bin/day1.rs
+++ 2015/src/bin/day1.rs
@@ -1,13 +1,7 @@
-use std::io::prelude::*;
 use utils;
 
 fn main() {
-  let mut file = utils::read_input_file();
-
-  let mut string = String::new();
-  file
-    .read_to_string(&mut string)
-    .expect("Failed to read input file as string");
+  let string = utils::read_input_file_as_string();
 
   let mut floor = 0;
   let mut first_basement_position = 0;
--- 2015/src/utils.rs
+++ 2015/src/utils.rs
@@ -2,23 +2,34 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
 struct Cli {
-    path: std::path::PathBuf,
+  path: std::path::PathBuf,
 }
 
 pub fn read_input_file() -> BufReader<File> {
-    let path = std::env::args().nth(1).expect("no path given");
-    let args = Cli {
-        path: std::path::PathBuf::from(path),
-    };
+  let path = std::env::args().nth(1).expect("no path given");
+  let args = Cli {
+    path: std::path::PathBuf::from(path),
+  };
 
-    let file = File::open(&args.path).expect("could not open file");
-    BufReader::new(file)
+  let file = File::open(&args.path).expect("could not open file");
+  BufReader::new(file)
 }
 
 pub fn read_input_file_lines() -> impl std::iter::Iterator<Item = String> {
-    let reader = read_input_file();
+  let reader = read_input_file();
 
-    reader
-        .lines()
-        .map(|line| line.expect("failed to read line"))
+  reader
+    .lines()
+    .map(|line| line.expect("failed to read line"))
+}
+
+pub fn read_input_file_as_string() -> String {
+  let mut file = read_input_file();
+
+  let mut string = String::new();
+  file
+    .read_to_string(&mut string)
+    .expect("Failed to read input file as string");
+
+  string
 }
