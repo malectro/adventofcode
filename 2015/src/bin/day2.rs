cb3d5d67003dbc47db51c86e41876693273560cc
Kyle Warren <qarren@gmail.com>
2021-12-26 21:45:30 -0800

    day 2 part 2

--- 2015/src/bin/day2.rs
+++ 2015/src/bin/day2.rs
@@ -1,11 +1,12 @@
 use std::cmp::min;
-use std::io::prelude::*;
 use utils;
 
 fn main() {
-  let mut file = utils::read_input_file_lines();
+  let file = utils::read_input_file_lines();
 
-  let mut total = 0;
+  let mut total_area = 0;
+  let mut total_ribbon = 0;
+
   for line in file {
     let size: Vec<usize> = line
       .split('x')
@@ -13,13 +14,19 @@ fn main() {
       .collect();
 
     let (w, h, l) = (size[0], size[1], size[2]);
-    let sides = [w * h, h * l, l * w];
 
+    let sides = [w * h, h * l, l * w];
     let smallest = sides.into_iter().fold(usize::MAX, |acc, val| min(acc, val));
     let surface_area = sides.iter().fold(0, |acc, val| val * 2 + acc);
 
-    total += surface_area + smallest;
+    total_area += surface_area + smallest;
+
+    let ribbon_length = [w + h, h + l, l + w]
+      .into_iter()
+      .fold(usize::MAX, |acc, val| min(acc, val));
+    total_ribbon += w * h * l + 2 * ribbon_length;
   }
 
-  println!("total wrapping paper required: {}", total);
+  println!("total wrapping paper required: {}", total_area);
+  println!("total ribbon required: {}", total_ribbon);
 }
