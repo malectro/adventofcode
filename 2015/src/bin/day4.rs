dd4a9452beea111529c69f70237e599f07581231
Kyle Warren <qarren@gmail.com>
2021-12-28 08:39:01 -0800

    day 4 part 2

--- 2015/src/bin/day4.rs
+++ 2015/src/bin/day4.rs
@@ -4,24 +4,36 @@ fn main() {
   let secret: String = std::env::args().nth(1).expect("no secret");
 
   let mut i = 0;
-  let mut found = 0;
-  while found == 0 {
+  let mut found_5 = 0;
+  let mut found_6 = 0;
+  while found_6 == 0 || found_5 == 0 {
     let digest = Md5::digest(format!("{}{}", secret, i));
-    if has_5_zeros(&digest) {
-      found = i;
+    let zero_count = count_zeros(&digest);
+    if zero_count >= 5 && found_5 == 0 {
+      found_5 = i;
+    }
+    if zero_count >= 6 && found_6 == 0 {
+      found_6 = i;
     }
     i += 1;
   }
 
   println!("your secret {}", secret);
-  println!("your salt {}", found);
+  println!("salt for 5 {}", found_5);
+  println!("salt for 6 {}", found_6);
 }
 
-fn has_5_zeros(digest: &[u8]) -> bool {
-  for j in 0..2 {
-    if digest[j] != 0 {
-      return false;
+fn count_zeros(digest: &[u8]) -> usize {
+  let mut count = 0;
+  for value in digest {
+    if *value == 0 {
+      count += 2;
+    } else {
+      if *value < 16 {
+        count += 1;
+      }
+      return count;
     }
   }
-  digest[2] < 16
+  count
 }
