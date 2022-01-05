f8b6fe5ed6a18e308bf180f9d21be519fc82c3ae
Kyle Warren <qarren@gmail.com>
2022-01-05 13:56:24 -0800

    day 18 part 2

--- 2015/src/bin/day18.rs
+++ 2015/src/bin/day18.rs
@@ -1,4 +1,5 @@
 use std::cmp;
+use std::collections::HashSet;
 use utils;
 
 fn main() {
@@ -8,9 +9,20 @@ fn main() {
 
   let grid_size = grid.len();
 
+  let mut final_grid = grid;
+  let corners = HashSet::from([
+    [0, 0],
+    [0, grid_size - 1],
+    [grid_size - 1, 0],
+    [grid_size - 1, grid_size - 1],
+  ]);
+
+  for [x, y] in corners.iter() {
+    final_grid[*y][*x] = true;
+  }
+
   //println!("{}", serialize_grid(&grid));
 
-  let mut final_grid = grid;
   for _ in 0..100 {
     let mut new_grid = final_grid.clone();
 
@@ -18,23 +30,27 @@ fn main() {
       for x in 0..grid_size {
         let light = final_grid[y][x];
 
-        let mut neighbor_count = 0;
+        if corners.contains(&[x, y]) {
+          new_grid[y][x] = true;
+        } else {
+          let mut neighbor_count = 0;
 
-        let mx = if x > 0 { x - 1 } else { x };
-        let my = if y > 0 { y - 1 } else { y };
+          let mx = if x > 0 { x - 1 } else { x };
+          let my = if y > 0 { y - 1 } else { y };
 
-        for nx in mx..cmp::min(grid_size, x + 2) {
-          for ny in my..cmp::min(grid_size, y + 2) {
-            if (nx != x || ny != y) && final_grid[ny][nx] {
-              neighbor_count += 1;
+          for nx in mx..cmp::min(grid_size, x + 2) {
+            for ny in my..cmp::min(grid_size, y + 2) {
+              if (nx != x || ny != y) && final_grid[ny][nx] {
+                neighbor_count += 1;
+              }
             }
           }
-        }
 
-        if light {
-          new_grid[y][x] = neighbor_count == 2 || neighbor_count == 3;
-        } else {
-          new_grid[y][x] = neighbor_count == 3;
+          if light {
+            new_grid[y][x] = neighbor_count == 2 || neighbor_count == 3;
+          } else {
+            new_grid[y][x] = neighbor_count == 3;
+          }
         }
       }
     }
