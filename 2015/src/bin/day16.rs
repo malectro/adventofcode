4034e615022aba3bc7ed43386bc88dc7f4e9d909
Kyle Warren <qarren@gmail.com>
2022-01-05 12:02:32 -0800

    day 16 part 2

--- 2015/src/bin/day16.rs
+++ 2015/src/bin/day16.rs
@@ -29,14 +29,35 @@ perfumes: 1";
     let (left, right) = line.split_once(": ").expect("Invalid line");
     let (_, id) = left.split_once(' ').expect("Invalid line");
 
-    let is_valid = right.split(", ").all(|pair| {
-      let (attr, value) = pair.split_once(": ").expect("Invalid attribute pair");
-      let count: usize = value.parse().expect("Invalid value");
-      Some(&count) == info.get(attr)
+    let attributes: Vec<(&str, usize)> = right
+      .split(", ")
+      .map(|pair| {
+        let (attr, value) = pair.split_once(": ").expect("Invalid attribute pair");
+        let count: usize = value.parse().expect("Invalid value");
+        (attr, count)
+      })
+      .collect();
+
+    let is_valid = attributes
+      .iter()
+      .all(|(attr, count)| Some(count) == info.get(attr));
+
+    let is_part_2_valid = attributes.iter().all(|(attr, count)| {
+      let gift_count = info.get(attr).expect("Invalid attr");
+
+      match *attr {
+        "cats" | "trees" => count > gift_count,
+        "pomeranians" | "goldfish" => count < gift_count,
+        _ => count == gift_count,
+      }
     });
 
     if is_valid {
-      println!("{} is valid", id);
+      println!("{} is valid for part 1", id);
+    }
+
+    if is_part_2_valid {
+      println!("{} is valid for part 2", id);
     }
   }
 }
