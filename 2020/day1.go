82c002901695b7af0f5d14c05c9771d6baa6dc60
Kyle Warren <qarren@gmail.com>
2022-02-01 18:54:25 -0800

    cleanup

--- 2020/day1.go
+++ 2020/day1.go
@@ -9,10 +9,10 @@ func main() {
 	var numbers []uint64
 
 	s := bufio.NewScanner(os.Stdin)
-    for s.Scan() {
+	for s.Scan() {
 		number, _ := strconv.ParseUint(s.Text(), 10, 64)
 		numbers = append(numbers, number)
-    }
+	}
 
 	var pair []uint64
 	for next, hasMore := permutations(numbers, 3); hasMore; pair, hasMore = next() {
@@ -22,7 +22,7 @@ func main() {
 	}
 }
 
-func permutations(slice []uint64, size uint) (func () ([]uint64, bool), bool) {
+func permutations(slice []uint64, size uint) (func() ([]uint64, bool), bool) {
 	indices := make([]int, size)
 	permutation := make([]uint64, size)
 	l := len(slice)
@@ -31,20 +31,20 @@ func permutations(slice []uint64, size uint) (func () ([]uint64, bool), bool) {
 		indices[i] = i
 	}
 
-	return func () ([]uint64, bool) {
+	return func() ([]uint64, bool) {
 		index := int(size) - 1
 
 		indices[index] += 1
 
-		for indices[index] > l - (int(size) - 1 - index) - 1 && index > 0 {
-		  indices[index - 1] += 1
-		  for i := index; i < int(size); i += 1 {
-		  	indices[i] = indices[i - 1] + 1
-		  }
-		  index -= 1
+		for indices[index] > l-(int(size)-1-index)-1 && index > 0 {
+			indices[index-1] += 1
+			for i := index; i < int(size); i += 1 {
+				indices[i] = indices[i-1] + 1
+			}
+			index -= 1
 		}
 
-		hasMore := indices[0] < l - int(size)
+		hasMore := indices[0] < l-int(size)
 
 		for i, index := range indices {
 			permutation[i] = slice[index]
