fd27965407a7762ca05cbe1af40e5f65790c2764
Kyle Warren <qarren@gmail.com>
2022-12-03 21:55:32 -0600

    day1 part 2

--- 2022/src/bin/day1.rs
+++ 2022/src/bin/day1.rs
@@ -2,6 +2,11 @@ use std::cmp::max;
 use utils;
 
 fn main() {
+  part1();
+  part2();
+}
+
+fn part1() {
   let lines = utils::read_input_file_lines();
 
   let mut most_calories = 0;
@@ -16,4 +21,24 @@ fn main() {
   }
 
   println!("the most calories is {}", most_calories);
+}
+
+fn part2() {
+  let lines = utils::read_input_file_lines();
+
+  let mut top_3: Vec<usize> = vec![0; 3];
+  let mut calories: usize = 0;
+
+  for line in lines {
+    if line == "" {
+      top_3.push(calories);
+      top_3.sort_unstable_by(|a, b| b.cmp(a));
+      top_3.pop();
+      calories = 0;
+    } else {
+      calories += line.parse::<usize>().unwrap();
+    }
+  }
+
+  println!("the top 3 calories total is {}", top_3.iter().sum::<usize>());
 }
