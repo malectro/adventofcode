8dee6c74cfc8007ec77f2f6d2c94186478acb0bc
Kyle Warren <qarren@gmail.com>
2022-12-05 14:54:52 -0800

    day3 part 2

--- 2022/src/bin/day3.rs
+++ 2022/src/bin/day3.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::collections::HashSet;
 use utils;
 
@@ -24,15 +25,41 @@ fn part1() {
       }
     }
 
-    score += if repeat_byte >= 97 {
-      repeat_byte as usize - 97 + 1
-    } else {
-      repeat_byte as usize - 65 + 27
-    }
+    score += get_score(repeat_byte);
   }
 
   println!("final score is {}", score);
 }
 
 fn part2() {
+  let lines = utils::read_input_file_lines();
+
+  let mut score: usize = 0;
+
+  for group in &lines.chunks(3) {
+    let mut set: HashSet<u8> = HashSet::new();
+
+    for pack in group {
+      let group_set: HashSet<u8> = HashSet::from_iter(pack.as_bytes().iter().copied());
+
+      set = if set.len() == 0 {
+        group_set
+      } else {
+        set.intersection(&group_set).copied().collect()
+      };
+    }
+
+    let badge = set.iter().next().unwrap();
+    score += get_score(*badge);
+  }
+
+  println!("final score is {}", score);
+}
+
+fn get_score(byte: u8) -> usize {
+  return if byte >= 97 {
+    byte as usize - 97 + 1
+  } else {
+    byte as usize - 65 + 27
+  };
 }
