0cb0ead06a52ad283e7383f22c1a29f4155ae880
Kyle Warren <qarren@gmail.com>
2022-12-06 10:05:03 -0800

    day 6 part 2

--- 2022/src/bin/day6.rs
+++ 2022/src/bin/day6.rs
@@ -1,8 +1,15 @@
-use itertools::Itertools;
 use std::io::Read;
+use std::collections::HashSet;
+use std::collections::VecDeque;
+use itertools::Itertools;
 use utils;
 
 fn main() {
+    part1();
+    part2();
+}
+
+fn part1() {
   let bytes = utils::read_input_file().bytes();
 
   let mut index = 0;
@@ -19,4 +26,28 @@ fn main() {
   }
 
   println!("found marker at {}", index + 4);
+}
+
+fn part2() {
+  let size = 14;
+  let bytes = utils::read_input_file().bytes();
+
+  let mut window = VecDeque::new();
+  let mut index = 0;
+
+  for (i, byte) in bytes.map(|b| b.expect("failed to read byte")).enumerate() {
+    if window.len() == size {
+      window.pop_front();
+    }
+    window.push_back(byte);
+
+    let set: HashSet<u8> = HashSet::from_iter(window.clone());
+
+    if set.len() == size {
+      index = i;
+      break;
+    }
+  }
+
+  println!("found marker at {}", index + 1);
 }
