b33088dbca02c360fab3292350cb852676730f5f
Kyle Warren <qarren@gmail.com>
2022-12-13 12:26:31 -0800

    day 9 part 2

--- 2022/src/bin/day9.rs
+++ 2022/src/bin/day9.rs
@@ -1,4 +1,5 @@
 use euclid::*;
+use itertools::Itertools;
 use std::collections::HashMap;
 use utils;
 
@@ -7,10 +8,14 @@ type Point = Point2D<isize, Space>;
 type Dist = Vector2D<isize, Space>;
 
 fn main() {
+  pull_rope(2);
+  pull_rope(10);
+}
+
+fn pull_rope(knot_count: usize) {
   let mut cels: HashMap<Point, bool> = HashMap::new();
 
-  let mut head_pos = Point::origin();
-  let mut tail_pos = Point::origin();
+  let mut knots: Vec<Point> = vec![Point::origin(); knot_count];
 
   for line in utils::read_input_file_lines() {
     let chars: Vec<&str> = line.split(" ").collect();
@@ -26,23 +31,30 @@ fn main() {
     };
 
     for _ in 0..distance {
-      head_pos += vector;
+      knots[0] += vector;
 
-      let difference = head_pos - tail_pos;
-      let distance = difference.abs();
-      let normal = difference.clamp(vec2(-1, -1), vec2(1, 1));
+      for i in 1..knots.len() {
+        let head_pos = knots[i - 1];
+        let mut tail_pos = knots[i];
 
-      if distance.x + distance.y > 2 {
-        tail_pos += normal;
-      } else if distance.x > 1 {
-        tail_pos.x += normal.x;
-      } else if distance.y > 1 {
-        tail_pos.y += normal.y;
+        let difference = head_pos - tail_pos;
+        let distance = difference.abs();
+        let normal = difference.clamp(vec2(-1, -1), vec2(1, 1));
+
+        if distance.x + distance.y > 2 {
+          tail_pos += normal;
+        } else if distance.x > 1 {
+          tail_pos.x += normal.x;
+        } else if distance.y > 1 {
+          tail_pos.y += normal.y;
+        }
+
+        knots[i] = tail_pos;
+
+        //println!("head: {:?}, tail: {:?}", head_pos, tail_pos);
       }
 
-      // println!("head: {:?}, tail: {:?}", head_pos, tail_pos);
-
-      cels.insert(tail_pos, true);
+      cels.insert(knots[knots.len() - 1], true);
     }
   }
 
