cd180f15295801c2e8ebe4ba5399320a72d6e51d
Kyle Warren <qarren@gmail.com>
2023-12-05 11:31:31 -0800

    day 5 part 2

--- 2023/src/bin/day5.rs
+++ 2023/src/bin/day5.rs
@@ -6,6 +6,7 @@ use nom::{
   sequence::{pair, preceded, tuple},
   IResult,
 };
+use std::cmp;
 
 fn main() {
   part1();
@@ -18,18 +19,30 @@ fn part1() {
   let (_, numbers) =
     preceded(tag("seeds: "), separated_list0(space1, parse_usize))(&line1).expect("Invalid seeds");
 
-  let mut seeds = numbers;
+  let (_, ranges) =
+    preceded(tag("seeds: "), separated_list0(space1, parse_range))(&line1).expect("Invalid seeds");
 
+  let mut seeds = numbers;
   let mut next_seeds: Vec<usize> = Vec::new();
+
+  let mut seed_ranges = ranges;
+  let mut next_ranges: Vec<SeedRange> = Vec::new();
+
   for line in lines {
     if line == "" {
       next_seeds.append(&mut seeds);
       seeds = next_seeds;
       next_seeds = Vec::new();
+
+      next_ranges.append(&mut seed_ranges);
+      seed_ranges = next_ranges;
+      next_ranges = Vec::new();
+
     } else if let Some(range) =
       tuple((parse_usize, space1, parse_usize, space1, parse_usize))(&line).ok()
     {
       let (_, (to, _, from, _, size)) = range;
+
       let mut removed_count = 0;
       for (i, seed) in seeds.clone().into_iter().enumerate() {
         if seed >= from && seed < from + size {
@@ -38,10 +51,41 @@ fn part1() {
           removed_count += 1;
         }
       }
+
+      for range in seed_ranges.iter_mut() {
+        if from <= range.from && from + size > range.from {
+          let clip_point = cmp::min(range.to, from + size);
+
+          let new_range = SeedRange {
+            from: to + range.from - from,
+            to: to + clip_point - from,
+          };
+          next_ranges.push(new_range);
+
+          range.from = clip_point;
+        } else if from < range.to {
+          let clip_point = cmp::max(range.from, from);
+          let end_clip_point = cmp::min(range.to, from + size);
+
+          if clip_point < end_clip_point {
+            let new_range = SeedRange {
+              from: to + clip_point - from,
+              to: to + end_clip_point - from,
+            };
+            next_ranges.push(new_range);
+
+            range.to = clip_point;
+          }
+        }
+      }
+
+      // kinda wasteful
+      seed_ranges.retain(|range| range.to > range.from)
     }
   }
 
   next_seeds.append(&mut seeds);
+  next_ranges.append(&mut seed_ranges);
 
   println!(
     "Part 1 {}",
@@ -50,8 +94,37 @@ fn part1() {
       .reduce(|acc, v| if v < acc { v } else { acc })
       .expect("No final seeds")
   );
+
+  println!(
+    "Part 2 {}",
+    next_ranges
+      .iter()
+      .reduce(|acc, range| if range.from < acc.from { range } else { acc })
+      .expect("No final ranges")
+      .from,
+  );
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+struct SeedRange {
+  from: usize,
+  to: usize,
 }
 
 fn parse_usize(input: &str) -> IResult<&str, usize> {
   return map_res(digit1, str::parse)(input);
 }
+
+fn parse_range(input: &str) -> IResult<&str, SeedRange> {
+  return map_res(
+    tuple((parse_usize, space1, parse_usize)),
+    |(from, _, size)| {
+      Ok::<SeedRange, Never>(SeedRange {
+        from,
+        to: from + size,
+      })
+    },
+  )(input);
+}
+
+enum Never {}
