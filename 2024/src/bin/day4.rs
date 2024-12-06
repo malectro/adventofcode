88b9c45fa22af21add738aaa7779a7e0923ccefc
Kyle Warren <qarren@gmail.com>
2024-12-06 09:51:29 -0800

    day 4 part 2

--- 2024/src/bin/day4.rs
+++ 2024/src/bin/day4.rs
@@ -19,25 +19,33 @@ fn part1() {
   let rtarget = target.chars().rev().collect::<String>();
 
   let input = utils::read_input_file_as_string();
-  let strings = input.split('\n').filter(|string| string.len() > 0).collect_vec();
+  let strings = input
+    .split('\n')
+    .filter(|string| string.len() > 0)
+    .collect_vec();
 
   let mut total: usize = 0;
 
-  total += strings.iter().map(|string|
-    string.matches(target).count() + string.matches(&rtarget).count()
-  ).sum::<usize>();
+  total += strings
+    .iter()
+    .map(|string| string.matches(target).count() + string.matches(&rtarget).count())
+    .sum::<usize>();
 
   let row_len = strings.len();
   let col_len = strings[0].len();
-  let chars: Vec<Vec<char>> = strings.iter().map(|string| string.chars().collect()).collect();
+  let chars: Vec<Vec<char>> = strings
+    .iter()
+    .map(|string| string.chars().collect())
+    .collect();
 
-  let cols: Vec<String> = (0..col_len).map(|col| 
-    (0..strings.len()).map(|row| chars[row][col]).collect()
-  ).collect_vec();
+  let cols: Vec<String> = (0..col_len)
+    .map(|col| (0..strings.len()).map(|row| chars[row][col]).collect())
+    .collect_vec();
 
-  total += cols.iter().map(|string|
-    string.matches(target).count() + string.matches(&rtarget).count()
-  ).sum::<usize>();
+  total += cols
+    .iter()
+    .map(|string| string.matches(target).count() + string.matches(&rtarget).count())
+    .sum::<usize>();
 
   let mut diags = Vec::new();
   for start_row in 0..row_len {
@@ -57,10 +65,13 @@ fn part1() {
 
     diags.push(string_down);
 
-    while row != 0 && col != 0 {
+    row = start_row + 1;
+    col = start_col;
+
+    while row != 0 && col < col_len {
       row -= 1;
-      col -= 1;
       string_up.push(chars[row][col]);
+      col += 1;
     }
 
     diags.push(string_up);
@@ -83,28 +94,22 @@ fn part1() {
 
     diags.push(string_down);
 
-    while row != 0 && col != 0 {
+    row = row_len;
+    col = start_col;
+
+    while row != 0 && col < col_len {
       row -= 1;
-      col -= 1;
       string_up.push(chars[row][col]);
+      col += 1;
     }
 
     diags.push(string_up);
   }
 
-  println!("diags {:?}", diags);
-
-  total += diags.iter().map(|string|
-    string.matches(target).count() + string.matches(&rtarget).count()
-  ).sum::<usize>();
-
-
-  /*
-  total += (0..col_len).map(|col| {
-    let column = ColumnIterator::of(&strings, col).collect::<String>();
-    column.matches(target).count() + column.matches(&rtarget).count()
-  }).sum();
-*/
+  total += diags
+    .iter()
+    .map(|string| string.matches(target).count() + string.matches(&rtarget).count())
+    .sum::<usize>();
 
   println!("Part 1 {}", total);
 }
@@ -112,55 +117,33 @@ fn part1() {
 fn part2() {
   let mut total = 0;
 
+  let target = "MAS";
+  let rtarget = target.chars().rev().collect::<String>();
+
+  let input = utils::read_input_file_as_string();
+  let strings = input
+    .split('\n')
+    .filter(|string| string.len() > 0)
+    .map(|string| string.chars().collect_vec())
+    .collect_vec();
+
+  let col_len = strings[0].len();
+
+  for row in 0..(strings.len() - target.len() + 1) {
+    for col in 0..(col_len - target.len() + 1) {
+      let mut down = String::new();
+      for i in 0..target.len() {
+          down.push(strings[row + i][col + i]);
+      }
+      let mut up = String::new();
+      for i in 0..target.len() {
+          up.push(strings[row + target.len() - 1 - i][col + i]);
+      }
+      if (down == target || down == rtarget) && (up == target || up == rtarget) {
+        total += 1;
+      }
+    }
+  }
+
   println!("Part 2 {}", total);
 }
-
-/*
-struct ColumnIterator<'a> {
-  i: usize,
-  col: usize,
-  strings: &'a Vec<&'a [char]>,
-}
-
-impl<'a> ColumnIterator<'a> {
-  fn of(strings: &'a Vec<&'a str>, col: usize) -> ColumnIterator<'a> {
-    ColumnIterator { i: 0, col, strings: strings.m }
-  }
-}
-
-impl Iterator for ColumnIterator<'_> {
-  type Item = char;
-
-  fn next(&mut self) -> Option<Self::Item> {
-    if self.i < self.strings.len() {
-      let i = self.i;
-      self.i += 1;
-      return Some(self.strings[i][self.col]);
-    }
-    None
-  }
-}
-
-fn parse_mult_num(input: &str) -> IResult<&str, usize> {
-  map_res(verify(digit1, |digits: &str| digits.len() < 4), str::parse)(input)
-}
-
-fn parse_mult(input: &str) -> IResult<&str, usize> {
-  map(
-    delimited(
-      tag("mul("),
-      separated_pair(parse_mult_num, tag(","), parse_mult_num),
-      tag(")"),
-    ),
-    |(a, b)| a * b,
-  )(input)
-}
-
-fn parse_dont(input: &str) -> IResult<&str, &str> {
-  tag("don't()")(input)
-}
-
-fn parse_do(input: &str) -> IResult<&str, &str> {
-  tag("do()")(input)
-}
-*/
